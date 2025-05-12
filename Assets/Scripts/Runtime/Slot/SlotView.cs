d8a968bbe5a82ba8e8fc846aa1bdd72bd6f7fd5a
N-u-j-e-e <310895@mail.ru>
2025-05-13 00:19:06 +0300

    Fix: bug with click
    Fix: bug when pointerUp and pointerDown are far and have significant delay, but click is still triggered. Solution was in replacing OnPointerClick with OnPointerDown + OnPointerUp and manual distance/delay control

--- Assets/Scripts/Runtime/Slot/SlotView.cs
+++ Assets/Scripts/Runtime/Slot/SlotView.cs
@@ -7,8 +7,14 @@ namespace Wigro.Runtime
 {
     [DisallowMultipleComponent]
     public sealed class SlotView : MonoBehaviour,
-        IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+        IPointerDownHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
+        private const float _clickDistanceThreshold = 1f;
+        private const float _clickTimeThreshold = 0.3f;
+
+        private Vector2 _lastDownPosition;
+        private float _lastDownTime;
+
         [field: SerializeField] public Image Background { get; private set; }
         [field: SerializeField] public Image SelectionFrame{ get; private set; }
 
@@ -17,7 +23,22 @@ namespace Wigro.Runtime
         public event Action<SlotView, PointerEventData> OnDragEvent = delegate { };
         public event Action<SlotView, PointerEventData> OnEndDragEvent = delegate { };
 
-        public void OnPointerClick(PointerEventData eventData) => OnClickEvent(this);
+        // Unity seems to consider any Up after Down on the same object as a click,
+        // no matter how far in time and space were Down and Up, hence is this Down+Up separate processing
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _lastDownPosition = eventData.position;
+            _lastDownTime = Time.time;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            var isWithinClickDistance = (Vector2.Distance(eventData.position, _lastDownPosition) < _clickDistanceThreshold);
+            var isWithinClickTime = (Time.time - _lastDownTime < _clickTimeThreshold);
+
+            if ( isWithinClickDistance && isWithinClickTime)
+                OnClickEvent(this);
+        }
 
         public void OnBeginDrag(PointerEventData eventData) => OnBeginDragEvent(this, eventData);
 
@@ -32,5 +53,6 @@ namespace Wigro.Runtime
         private void ToggleFrame(bool isActive) => SelectionFrame.gameObject.SetActive(isActive);
 
         public void PaintBackground(Color toColor) => Background.color = toColor;
+
     }
 }
