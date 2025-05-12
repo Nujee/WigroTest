b6f08fc504ad13a181472b54373aab9b4b2a407a
N-u-j-e-e <310895@mail.ru>
2025-05-13 02:05:17 +0300

    VSERION 2.0: addressables added

--- Assets/Scripts/Runtime/Core/EntryPoint.cs
+++ Assets/Scripts/Runtime/Core/EntryPoint.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
 
 namespace Wigro.Runtime
 {
@@ -6,19 +9,32 @@ namespace Wigro.Runtime
     public sealed class EntryPoint : MonoBehaviour
     {
         [field: SerializeField] public Settings Settings { get; private set; }
-        [field: SerializeField] public InventoryView InventoryView { get; private set; }
+        [field: SerializeField] public AssetReference SceneReference { get; private set; }
 
-        // 11) c)
-        private void Start()
+        private async void Start()
         {
-            // 9)
-            var database = new DatabaseLoader().Database;
+            await LoadInventorySceneAsync();
 
-            var model = new InventoryModel();
-            var presenter = new InventoryPresenter(model, InventoryView);
+            var inventoryView = FindObjectOfType<InventoryView>();
 
-            InventoryView.Init(Settings, database.Count);
-            presenter.Init(Settings.Amount, database);
+            if (inventoryView != null)
+            {
+                var database = new DatabaseLoader().Database;
+                var model = new InventoryModel();
+                var presenter = new InventoryPresenter(model, inventoryView);
+
+                inventoryView.Init(Settings, database.Count);
+                presenter.Init(Settings.Amount, database);
+            }
+            else Debug.LogError("InventoryView not found in loaded scene.");
+        }
+
+        private async Task LoadInventorySceneAsync()
+        {
+            var asyncOperation = SceneReference.LoadSceneAsync(LoadSceneMode.Additive);
+
+            while (!asyncOperation.IsDone)
+                await Task.Yield();
         }
     }
 }
